18dd170a966d59900d08d8485809f10ad035a1ff
pshemke <67806185+pshemke@users.noreply.github.com>
2024-06-12 21:33:11 +0200

    dziala parametryzacja

--- train.py
+++ train.py
@@ -1,79 +1,55 @@
+import typer
 import tensorflow as tf
 import os
-import cv2
 import numpy as np
-import matplotlib.pyplot as plt
-import random
-import pickle
-from sklearn.metrics import accuracy_score,precision_score,recall_score
+from typing_extensions import Annotated
 import mlflow
 
-DATA_PATH='data/PotatoPlants'
-CLASSES=os.listdir(DATA_PATH)
-CLASSES.sort()
-IMG_WIDTH=300
-IMG_HEIGHT=300
-RGB=3
-NUM_CLASSES=len(CLASSES)
-EPOCHS=1
+def main(batch_size: int = typer.Option(32, "--batch_size", "-b", help="Batch size for training"),
+         learning_rate: float = typer.Option(1e-4, "--learning_rate", "-lr", help="Learning rate for optimizer"),
+         epochs: int = typer.Option(1, "--epochs", "-e", help="Number of epochs")):
+    DATA_PATH='data/PotatoPlants'
+    CLASSES=os.listdir(DATA_PATH)
+    IMG_WIDTH=300
+    IMG_HEIGHT=300
+    NUM_CLASSES=len(CLASSES)
 
-# import data
-data=tf.keras.utils.image_dataset_from_directory(DATA_PATH,
-                                                 image_size=(IMG_WIDTH,IMG_HEIGHT))
+    data=tf.keras.utils.image_dataset_from_directory(DATA_PATH, image_size=(IMG_WIDTH, IMG_HEIGHT))
+    data=data.map(lambda x, y: (x/255, y))
 
-# data augmentation to model
-data_augmentation = tf.keras.Sequential([
-    tf.keras.layers.experimental.preprocessing.RandomFlip("horizontal", 
-                                                 input_shape=(IMG_HEIGHT, 
-                                                              IMG_WIDTH,
-                                                              RGB)),
-    tf.keras.layers.experimental.preprocessing.RandomRotation(0.1),
-    tf.keras.layers.experimental.preprocessing.RandomZoom(0.1),
-])
+    train_size = int(len(data)*0.8)
+    val_size = int(len(data)*0.1)
+    test_size = int(len(data)*0.1)
 
-# scaling data and making batches iterable
-data=data.map(lambda x,y: (x/255,y))
+    train = data.take(train_size)
+    val = data.skip(train_size).take(val_size)
+    test = data.skip(train_size+val_size).take(test_size)
 
-# divide data into training, validation and testing
-train_size = int(len(data)*0.8)
-val_size = int(len(data)*0.1)
-test_size = int(len(data)*0.1)
+    model=tf.keras.Sequential([
+        tf.keras.layers.Conv2D(16, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Conv2D(32, (3,3), activation='relu'),
+        tf.keras.layers.MaxPooling2D(),
+        tf.keras.layers.Dropout(0.1),
+        tf.keras.layers.Flatten(),
+        tf.keras.layers.Dense(128, activation='relu'),
+        tf.keras.layers.Dense(NUM_CLASSES)    
+    ])
 
-train = data.take(train_size)
-val = data.skip(train_size).take(val_size)
-test = data.skip(train_size+val_size).take(test_size)
+    model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=learning_rate),
+                  loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), 
+                  metrics=['accuracy'])
 
-# creating model
-model=tf.keras.Sequential([
-    #data_augmentation,
-    tf.keras.layers.Conv2D(16, (3,3),1, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
+    with mlflow.start_run():
+        history = model.fit(train, epochs=epochs, validation_data=val, batch_size=batch_size)
+        
+        mlflow.log_param("epochs", epochs)
+        mlflow.log_param("batch_size", batch_size)
+        mlflow.log_param("learning_rate", learning_rate)
+        mlflow.log_metric("accuracy", history.history['accuracy'][-1])
+        mlflow.log_metric("val_accuracy", history.history['val_accuracy'][-1])
+        mlflow.set_tracking_uri("http://localhost:5000")
+        mlflow.keras.log_model(model, "model")
 
-    tf.keras.layers.Conv2D(32, (3,3),1, activation='relu'),
-    tf.keras.layers.MaxPooling2D(),
-    
-    tf.keras.layers.Dropout(0.1),
-    tf.keras.layers.Flatten(),
-    tf.keras.layers.Dense(128, activation='relu'),
-    tf.keras.layers.Dense(NUM_CLASSES)    
-])
-
-# compiling model
-model.compile(optimizer=tf.keras.optimizers.RMSprop(learning_rate=1e-4),
-               loss=tf.losses.SparseCategoricalCrossentropy(from_logits=True), 
-               metrics=['accuracy']
-               )
-
-with mlflow.start_run():
-    history = model.fit(
-        train,
-        epochs=EPOCHS,
-        validation_data=val,
-    )
-    
-    mlflow.log_param("epochs", EPOCHS)
-    mlflow.log_param("batch_size", 32)
-    mlflow.log_metric("accuracy", history.history['accuracy'][-1])
-    mlflow.log_metric("val_accuracy", history.history['val_accuracy'][-1])
-    
-    mlflow.keras.log_model(model, "model")
+if __name__ == "__main__":
+    typer.run(main)
